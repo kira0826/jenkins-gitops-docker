6c599d22b3f12952d3c38853226f7d46a9f18b6d
zai <curaca492@gmail.com>
2025-05-13 00:36:43 -0500

    change to test integration.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello ArgoCD is Cool cambio mi papá'
+    return 'Hello ArgoCD is cambio mi papá'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
